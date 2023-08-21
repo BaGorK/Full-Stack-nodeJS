5def60fecf5f3eea34ee521a435d436d91770263
BaGorK <edmealemkassahun@gmail.com>
2023-08-21 00:52:21 -0700

    Validation to our products updated

--- 04-persistence/01/products.js
+++ 04-persistence/01/products.js
@@ -1,19 +1,33 @@
 const cuid = require('cuid');
+const { isURL } = require('validator');
 
 const db = require('./db');
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
+
+const urlSchema = (opts = {}) => {
+  const { required } = opts;
+
+  return {
+    type: String,
+    required: !!required,
+    validate: {
+      validator: isURL,
+      message: (props) => `${props.value} is not a valid URL `,
+    },
+  };
+};
 
 const Product = db.model('Product', {
   _id: { type: String, default: cuid },
-  description: String,
-  imgThumb: String,
-  img: String,
-  link: String,
-  userId: String,
-  userName: String,
-  userLink: String,
-  tags: { type: String, index: true },
+  description: { type: String, required: true },
+  imgThumb: urlSchema({required: true}),
+  img: urlSchema({required: true }),
+  link: urlSchema(),
+  userId: { type: String, required: true },
+  userName: { type: String, required: true },
+  userLink: urlSchema(),
+  tags: { type: [String], index: true },
 });
 
 const create = async (fields) => {
