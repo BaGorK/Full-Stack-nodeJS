fbefbab2d6a4a4b7394df1f97ce05d8180744ea8
BaGorK <edmealemkassahun@gmail.com>
2023-08-24 04:09:07 -0700

    creating auth module

---
+++ 05-authentication/02-creating-auth-module/server.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const api = require('./api');
+const auth = require('./auth');
+const middleware = require('./middleware');
+
+const port = process.env.PORT;
+
+app.use(middleware.cors);
+app.use(bodyParser.json());
+app.use(cookieParser());
+auth.setMiddleware(app);
+
+app.post('/login', auth.authenticate, auth.login);
+
+app.get('/products', api.listProducts);
+app.get('/products/:id', api.getProduct);
+app.post('/products', auth.ensureAdmin, api.createProduct);
+app.put('/products/:id', auth.ensureAdmin, api.editProduct);
+app.delete('/products/:id', auth.ensureAdmin, api.deleteProduct);
+
+app.get('/orders', auth.ensureAdmin, api.listOrders);
+app.post('/orders', auth.ensureAdmin, api.createOrder);
+
+app.use(middleware.handleValidationError);
+app.use(middleware.handleError);
+app.use(middleware.notFound);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
